f3ff168d4e68daa9555bfa89e94747ed45880119
Paul Ouellette PC <pouellette25@gmail.com>
2017-05-28 09:02:14 -0400

    Removed content not being used. Added new gamemode class

---
+++ Source/Outlanders/GameModes/OutlandersGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "OutlandersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OUTLANDERS_API AOutlandersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
